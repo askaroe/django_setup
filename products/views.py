3e5760d3c1187770d0a354cc37942e8075484205
Askar Oralkhan <askar4ik123@gmail.com>
2024-03-29 12:59:01 +0500

    api initialize

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+@api_view()
+def say_hello(request):
+    queryset = Product.objects.all()
+    # print(list(queryset))
+    # list(queryset)
+    # for i in queryset:
+    #     print(i)
+    # queryset[0:5]
+
+    # return HttpResponse('Hello, Django!')
+    # return render(request, 'hello.html', context={
+    #     'name': 'Askar',
+    #     'products': list(queryset),
+    # })
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
